dc01df4ebe5378ec38b09fb70a76a183aa6589c1
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-28 20:45:05 +0300

    d

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,7 +3,6 @@
 from typing import Tuple
 
 
-def index_rang(page: int, page_size: int) -> Tuple[int, int]
-"""Retrieves the index range from a given page and page size"""
-
-return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
+def index_rang(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size"""
+    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
