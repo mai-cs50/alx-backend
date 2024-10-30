6b1d54efb8eb4d0d2a9193c58eae3593f020acfd
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-30 17:27:22 +0300

    d

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -8,25 +8,25 @@ from base_caching import BaseCaching
 
 class FIFOCache(BaseCaching):
     """A class 'FIFOCache' that inherits from
-       'BaseCaching' and is a caching system"""
+    'BaseCaching' and is a caching system"""
 
-       def __init__(self):
-           super().__init__()
-           self.cache_data = OrderedDict()
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
 
-        def put(self, key, item):
-            """assign to the dictionary 'self.cach_data' the
-               'item' value for the key 'key'"""
+    def put(self, key, item):
+        """assign to the dictionary 'self.cach_data' the
+        'item' value for the key 'key'"""
 
-               if key is None or item is None:
-                   return
+        if key is None or item is None:
+            return
 
-               if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                   first_key, _ = self.cache_data.popitem(last=False)
-                   print(f"DISCARD: {frist_key}")
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {frist_key}")
 
-                self.cache_data[key] = item
+        self.cache_data[key] = item
 
-        def get(self, key):
-            """return the value in 'self.cache_data' linked to 'key'"""
-            return self.cache_data.get(key, None)
+    def get(self, key):
+        """return the value in 'self.cache_data' linked to 'key'"""
+        return self.cache_data.get(key, None)
