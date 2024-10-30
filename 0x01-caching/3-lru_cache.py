ce455ac1bb05cb9f8d53d4462b4042af33fc5a5d
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-30 18:49:39 +0300

    d

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -7,7 +7,7 @@ from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """A class 'FIFOCache' that inherits from
+    """A class 'LRUCache' that inherits from
     'BaseCaching' and is a caching system"""
 
     def __init__(self):
@@ -16,23 +16,22 @@ class LRUCache(BaseCaching):
        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """assign to the dictionary 'self.cach_data' the
-        'item' value for the key 'key'"""
+        """Adds an item in the cache"""
 
         if key is None or item is None:
             return
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                Lru_key, _ = self.cache_data.popitem(True)
+                lru_key, _ = self.cache_data.popitem(True)
                 print("DISCARD:", lru_key)
 
             self.cache_data[key] = item
-            self.cache_data.move_to_end(key, last=True)
+            self.cache_data.move_to_end(key, last=False)
         else:
             self.cache_data[key] = item
 
     def get(self, key):
         """Retrieves an item by key"""
-        if key is not None key in self.cache_data:
+        if key is not None and key in self.cache_data:
             self.cache_data.move_to_end(key, last=False)
         return self.cache_data.get(key, None)
