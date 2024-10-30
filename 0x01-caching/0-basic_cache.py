2d04d78dbc3fbb6c14a8fbe4037dd9ce2a7a8d88
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-30 17:23:38 +0300

    d

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,15 +7,16 @@ from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
     """A cass 'BasicCach' that inherits from 'BaseCaching'
-       and is a caching system"""
+       and is a caching system
+    """
 
-       def put(self, key, item):
-           """assign to the dictionary 'self.cach_data' the
-              'item' value for the keu 'key'"""
-              if key is not None and item is not None:
-                  self.cache_data[key] = item
+    def put(self, key, item):
+        """assign to the dictionary 'self.cach_data' the
+        'item' value for the keu 'key'"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
 
-        def get(self, key):
-            """return the value in 'self.cach_data' linked to 'key'"""
+    def get(self, key):
+        """return the value in 'self.cach_data' linked to 'key'"""
 
-            return self.cache_data.get(key, None)
+        return self.cache_data.get(key, None)
---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Task 3: LRU caching"""
+
+
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """A class 'FIFOCache' that inherits from
+       'BaseCaching' and is a caching system"""
+
+       def __init__(self):
+           """Initializes the cache"""
+           super().__init__()
+           self.cache_data = OrderedDict()
+
+        def put(self, key, item):
+            """assign to the dictionary 'self.cach_data' the
+               'item' value for the key 'key'"""
+
+               if key is None or item is None:
+                   return
+               if key not in self.cache_data:
+                   if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                       Lru_key, _ = self.cache_data.popitem(True)
+                       print("DISCARD:", lru_key)
+
+                    self.cache_data[key] = item
+                    self.cache_data.move_to_end(key, last=True)
+                else:
+                    self.cache_data[key] = item
+
+        def get(self, key):
+            """Retrieves an item by key"""
+            if key is not None key in self.cache_data:
+                self.cache_data.move_to_end(key, last=False)
+            return self.cache_data.get(key, None)
